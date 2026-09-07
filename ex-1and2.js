eeacfb5d40dadb55b9d15a51f1bcb9611d381c2e
JR <Jutarat.rng@gmail.com>
2026-09-07 20:44:30 +0700

    fix: ex-1 & ex-2

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,23 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let userName = 'Kartar';
+console.log(userName);
+let userSurname = 'Anderson';
+console.log(userSurname);
+let userAddress = '54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai';
+console.log(userAddress);
+const restaurantName = "Chef's Table by Chef James";
+console.log(restaurantName);
+const foodBills = 15000;
+console.log(foodBills);
+const openRestaurant = 1500;
+console.log(openRestaurant);
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+userAddress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+console.log(userAddress);
+foodBills = 5000;
+console.log(foodBills);
+openRestaurant = 2500;
+console.log(openRestaurant);
